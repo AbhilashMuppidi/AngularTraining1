f0c45bed38c56adfb3bd493d258eec66061b47ac
Abhilash <abhilash.muppidi@gmail.com>
2018-01-11 12:55:46 +0530

    Template forms for 2 way binding.

--- src/app/shopping/categories-list.component.ts
+++ src/app/shopping/categories-list.component.ts
@@ -5,15 +5,23 @@ import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
     template: `
-    <form [formGroup]="myform">
+    <!--<form [formGroup]="myform">
         <input type="text" placeholder="Enter Category Name"
          formControlName="name">
+    </form>    
+    -->
+    <form #myform="ngForm">
+        <input type="text" placeholder="Enter Category Name"
+            [(ngModel)]="frmCategory.name" name="txtName" required>
+        <button type="button" class="btn btn-primary"
+            [disabled]="!myform.valid">Add</button> 
     </form>
     <pre>{{myform.value|json}}</pre>
     <mytable tblTitle="List of Categories"
     [colHeadings]="headings" [data]="categories"></mytable>`
 })
 export class CategoriesListComponent {
+    frmCategory: Category=new Category(null,null);
     headings: string[] = [];
     categories: Array<Category> = [];
     ///private csvc:CategoryService=new CategoryService();
@@ -29,10 +37,10 @@ export class CategoriesListComponent {
         //this.categories=this.csvc.getCategories();
     }
     // Reactive Forms
-    myform: FormGroup;
-    ngOnInit() {
-        this.myform = new FormGroup({
-            "id": new FormControl(), "name": new FormControl()
-        });
-    }
+    // myform: FormGroup;
+    // ngOnInit() {
+    //     this.myform = new FormGroup({
+    //         "id": new FormControl(), "name": new FormControl()
+    //     });
+    // }
 }
